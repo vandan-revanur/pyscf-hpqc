118291c256f7bd7be6ba32534606694c58802bb6
vandan-revanur <vandanrevanur@gmail.com>
2023-04-11 11:22:47 +0200

    Remove the date modified in the regex for getting xyz info

--- pyscf/tools/get_xyz.py
+++ pyscf/tools/get_xyz.py
@@ -1,8 +1,8 @@
 import re
 
 def get_xyz(db_data:str, name:str)-> str:
-    p = re.compile(f'(NAME = {name})(.*?)Date modified:(.*?)\)(.*?)NAME', flags=re.DOTALL)
+    p = re.compile(f'NAME = {name}(.*?)NAME', flags=re.DOTALL)
     result = p.search(db_data)
 
-    xyz_info = result.group(4)
+    xyz_info = result.group(1)
     return xyz_info
